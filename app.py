4b43e33bea04e5584dfde1aea7364f68422e5945
arwa1arrm <arwa_mesloub@yahoo.com>
2025-04-29 01:28:54 +0300

    login connection

--- app.py
+++ app.py
@@ -1317,7 +1317,7 @@ def verify_otp():
 
             # Insert the user data into the database
             cur.execute("INSERT INTO `users`(`user_name`, `email`, `password`, `certificate`) VALUES (%s, %s, %s, %s)",
-                        (session['user_name'], session['email'], hashed_password.decode('utf-8'), session['certificate'].decode()))
+                        (session['user_name'], session['email'], hashed_password, session['certificate'].decode()))
             con.commit()
 
             # Store the user ID in the session
