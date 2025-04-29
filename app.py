8c703ef8d5f7dc8ab9becd6afabc6e84cfcab816
arwa1arrm <arwa_mesloub@yahoo.com>
2025-04-29 16:06:19 +0300

    login connection

--- app.py
+++ app.py
@@ -426,21 +426,37 @@ def encrypt_and_hide():
         # Retrieve receiver details
         cur.execute("SELECT user_id, certificate FROM users WHERE email = %s", (receiver_email,))
         receiver_data = cur.fetchone()
+
+        
+        
         if not receiver_data:
             flash("The receiver's email is not registered.", "danger")
             return redirect(url_for("encryptionPage"))
 
-        receiver_id, receiver_certificate_pem = receiver_data
-        receiver_certificate_pem = receiver_certificate_pem.replace('\\n', '\n')
-        receiver_certificate = x509.load_pem_x509_certificate(receiver_certificate_pem.encode(), default_backend())
+        # إصلاح تنسيق الشهادة
+        if "\\n" in receiver_certificate_pem:
+            receiver_certificate_pem = receiver_certificate_pem.encode('utf-8').decode('unicode_escape')
+        receiver_certificate_pem = receiver_certificate_pem.strip()
+
+        receiver_certificate = x509.load_pem_x509_certificate(
+            receiver_certificate_pem.encode('utf-8'), default_backend()
+        )
         receiver_public_key = receiver_certificate.public_key()
+
+       
+
 
         # Retrieve sender details
         cur.execute("SELECT certificate FROM users WHERE user_id = %s", (sender_id,))
         sender_certificate_pem = cur.fetchone()[0]
-        sender_certificate_pem = sender_certificate_pem.replace("\\n", "\n").strip()
-        sender_certificate = x509.load_pem_x509_certificate(sender_certificate_pem.encode('utf-8'),default_backend() )
+         # إصلاح تنسيق الشهادة
+        if "\\n" in sender_certificate_pem:
+            sender_certificate_pem = sender_certificate_pem.encode('utf-8').decode('unicode_escape')
+        sender_certificate_pem = sender_certificate_pem.strip()
 
+        sender_certificate = x509.load_pem_x509_certificate(
+            sender_certificate_pem.encode('utf-8'), default_backend()
+        )
         sender_public_key = sender_certificate.public_key()
 
         # Step 1: Encrypt the message with AES symmetric encryption
