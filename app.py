588c84fdfc3be26f5247ec10baff532782e75eb6
arwa1arrm <arwa_mesloub@yahoo.com>
2025-05-04 16:57:28 +0300

    login connection

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import re  # Regular expressions
 import time
 import zipfile
 from flask import Flask, render_template, session, url_for, request, redirect, send_file, flash, jsonify
-from flaskext.mysql import MySQL
+#from flaskext.mysql import MySQL
+from flask_mysql import MySQL
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization, hashes
